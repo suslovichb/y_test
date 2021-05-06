b822b8fb76b5f8768068f95697fc7339d9b0219e
suslovichb <suslovichb@gmail.com>
2021-05-06 17:38:23 +0300

    Update Course model

--- y_test/courses/models.py
+++ y_test/courses/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    lectures_number = models.PositiveIntegerField()
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    lectures_number = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.name
